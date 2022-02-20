770267eef4bd78401594f834f8e4b309832fe386
yanikisia <kathlyn.yary@gmail.com>
2022-02-20 11:48:12 -0300

    feita funçao de validar a data de nascimento

--- src/services/selectClass.ts
+++ src/services/selectClass.ts
@@ -2,5 +2,5 @@ import connection from "../data/connection"
 
 export const selectClass= async(id:string) =>{
     const result= await connection("class").select("id").where({id:id})
-    
-    return result;
+    return result
+}
