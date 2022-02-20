3a2007dda9e136b66c56bc6ce47a30ab2918cebc
yanikisia <kathlyn.yary@gmail.com>
2022-02-20 12:45:21 -0300

    pequenos ajustes

--- src/services/SelectIdSpecialty.ts
+++ src/services/SelectIdSpecialty.ts
@@ -2,6 +2,5 @@ import connection from "../data/connection"
 
 export const selectIdSpecialty= async(name:string): Promise<any> =>{
     const result= await connection("specialty").select("id").where({name:name})
-console.log('resultispeci',result)
     return result
 }
